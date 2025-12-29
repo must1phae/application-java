a495e93eb3bb9e937aa32e8af51e5431c9ebc057
mustapha <mostaphaelghazal102030@gmail.com>
2025-12-29 12:38:23 +0100

    j ai ajouter quelque modificcation

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException; // Import obligatoire pour l'erreur
+import java.io.IOException; // gestion des erreurs
 
 public class Main extends Application {
 
